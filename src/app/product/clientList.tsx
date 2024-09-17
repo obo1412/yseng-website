f3ce09498a92363ec993faed3d39a5d644e15c4b
obo1412 <obo1412@gmail.com>
2024-09-18 00:58:56 +0900

    고객사 정보 객체화 정리

--- src/app/product/clientList.tsx
+++ src/app/product/clientList.tsx
@@ -2,9 +2,27 @@ export const clientList = {
   domestic: [
     {
       client: "DY AUTO",
-      type: {
-        line: [],
-      },
+      type: [
+        {
+          name: "ARMATURE LOTOR 생산라인 - ATR",
+          line: ["#6", "#7"],
+        },
+        {
+          name: "POWER WINDOW MOTOR 생산라인",
+          line: [
+            "#3",
+            "#6",
+            "#7",
+            "#8",
+            "#9",
+            "#10",
+            "#11",
+            "#12",
+            "#13",
+            "#14",
+          ],
+        },
+      ],
     },
   ],
   global: [],
