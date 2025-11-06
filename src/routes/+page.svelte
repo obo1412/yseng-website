5a78376281ccf2cf2104ae761e58eec808caf811
obo1412 <obo1412@gmail.com>
2025-11-07 00:00:13 +0900

    메인화면 고정으로 우선 띄우기

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,3 @@
-<script>
-	import Carousel from '$lib/components/Carousel.svelte';
-	import { onMount } from 'svelte';
-
-	let images = [];
-
-	onMount(async () => {
-		const res = await fetch('/api/images');
-		if (res.ok) {
-			images = await res.json();
-		}
-	});
-</script>
-
-<Carousel {images} />
+<div class="flex w-full justify-center">
+	<img src="/indexPicture/yseng-main1.jpeg" alt="YS ENG Main" />
+</div>
