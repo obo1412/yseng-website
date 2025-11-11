dff46222d0d7830a81592e90fd604d6c46f9bada
obo1412 <obo1412@gmail.com>
2025-11-11 23:12:32 +0900

    회사소개 및 고객사 & 납품설비 이력 기입

--- src/routes/clients/+page.svelte
+++ src/routes/clients/+page.svelte
@@ -1,1 +1,108 @@
-<h1>고객사</h1>
+<div>
+	<h1>고객사</h1>
+	<div>
+		<h2>국내 고객사</h2>
+		<span>삼화정밀</span>
+		<span>ITOPS AUTOMOTIVE</span>
+		<span>세우정밀</span>
+		<span>삼성전기</span>
+		<span>보그워너</span>
+		<span>주성ENG</span>
+		<span>경원정공</span>
+		<span>DY AUTO</span>
+		<span>LG이노텍</span>
+	</div>
+	<div>
+		<h2>해외 고객사</h2>
+		<span>DY AUTO 중국 연태 공장</span>
+		<span>DY AUTO 인도 Delhi 공장</span>
+		<span>DY AUTO 멕시코 Apodaca 공장</span>
+	</div>
+</div>
+<div>
+	<h1>설비 납품 이력(국내)</h1>
+	<div>
+		<h2>디와이오토</h2>
+		<div>
+			라인설비 ARMATURE; LOTOR 생산라인 ATR #6, #7 LINE POWER WINDOW MOTOR 생산라인 PWM #3, #6, #7,
+			#8, #9, #10, #11, #12, #13, #14 LINE FRONT WIPER MOTOR 생산라인 FWM #1, #2, #3 LINE REAR WIPER
+			MOTOR 생산라인 RWM #2, #3 LINE COOLING FAN MOTOR 생산라인 CFM #1, #3, #5 LINE BLDC COOLING FAN
+			MOTOR 생산라인 BLCD #1 LINE
+		</div>
+		<div>
+			단독설비 스테이터-마그네트 본딩기 샤프트 콕킹 & 스틸볼 삽입기 수동 체결기 정류자 & 링 마그네트
+			수동 삽입 & 전장검사기. 아마추어 버핑, 브러쉬, 터닝기, 에이징 M/C 와이퍼 모터 스테이터 콕킹
+			M/C, 핫멜트 콘베어 M/C 다이아후램 3축 체결기, 본드 도포기 델타 리어 와이퍼 수동 조립 라인
+		</div>
+	</div>
+	<div>
+		<h2>아이탑스 오토모티브</h2>
+		<div>
+			ACTUATOR CURLING M/C 화약식 ACTUATOR 조립라인 화약식 2세대 ACTUATOR 조립라인 T1xx ACTUATOR
+			조립라인 GM BEV2 ACTUATOR 조립라인 PTG SPINDLE MOTOR 조립라인 #1 PTG SPINDLE MOTOR 조립라인 #2
+			TOUCH PAD SWITCH 조립라인 APS 3M TAPE 부착설비 RS4차종 센서조립 라인
+		</div>
+	</div>
+	<div>
+		<h2>두원공조</h2>
+		<div>전동 콤푸레셔 모터 ROTOR #1 LINE, 전동 콤푸레셔 모터 ROTOR #2 LINE</div>
+	</div>
+	<div>
+		<h2>도남정공</h2>
+		<div>
+			샤프트 공급, 세척, 널링장치 1, 2, 3, 4, 5호기 정지연마기 샤프트 공급 & 적재 장치 1, 2, 3, 4,
+			5호기 통과연마기 샤프트 공급 & 적재 장치 1, 2, 3, 4, 5, 6, 7호기 스크레칭 프레스 1, 2, 3호기
+			샤프트 가공라인 스틸볼 삽입기 샤프트 커팅기 리드 스크류 취출 장치
+		</div>
+	</div>
+	<div>
+		<h2>LS Automotive(구 대성전기)</h2>
+		<div>스크류 자동 체결기 TAS MAGNET 조립라인 48V CVT ASS'Y 조립라인</div>
+	</div>
+	<div>
+		<h2>BORG-WARNERS BERU KOREA SYSTEMS</h2>
+		<div>
+			이그네이션 코일 조립 M/C 펜슬코일 조립기 오토TM 솔레노이드 조립기 굴루프러그 동심도 검사기
+			양방향 워셔 펌프 모터 조립M/C 에폭시 예열 챔버 HEAD LAMP 솔레노이드 성능 검사
+		</div>
+	</div>
+	<div>
+		<h2>세우정밀</h2>
+		<div>
+			정지연마기 샤프트 공급 & 적재 장치 1, 2, 3, 4, 5, 6, 7, 8호기 통과연마기 샤프트 공급 & 적재
+			장치 1, 2호기
+		</div>
+	</div>
+	<div>
+		<h2>기가테크</h2>
+		<div>샤프트 가공라인 (공급, 세척, SKIVE) SKIVE 단독설비 1, 2, 3호기</div>
+	</div>
+	<div>
+		<h2>경원정공</h2>
+		<div>SKIVE 단독설비 1, 2, 3호기</div>
+	</div>
+	<div>
+		<h2>삼성전기</h2>
+		<div>
+			실리콘도포기 스피커 조립 자동라인 자동함침기 바디본딩M/C 금사선절단기 SMD탭핑M/C 오토마킹기
+			테스팅M/C 세라믹 씨트 분쇄기 본드 자동 토출기 2액정량 배합 토출기 샌드 테스트기
+		</div>
+	</div>
+	<div>
+		<h2>LG이노텍(구 LG마이크론)</h2>
+		<div>
+			피에조 부저 UV 접착기 타원형 도포기 단부락카 도포 시스템 단부 도포 시스템 액절 장치 LF현상C2
+		</div>
+	</div>
+	<div>
+		<h2>삼화정밀</h2>
+		<div>바디 세척 및 부쉬 조립 M/C 도어 힌지 특성 시험기 도어 힌지 자동 조립 라인</div>
+	</div>
+</div>
+<div>
+	<h1>설비 납품 이력(해외)</h1>
+	<div>
+		중국 - 연태 ATR #2, #3 LINE PWM #3, #4, #5 LINE 인도 - Delhi ATR #1 LINE DPM #1 LINE PWM #1,
+		#2,#3 LINE END-BRACKET 조립 LINE 멕시코 - Apodaca PWM #1, #2 LINE RWM #1 LINE ATR #1 LINE
+	</div>
+</div>
