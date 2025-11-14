d1a70f23a773f645a69776e2bb3f494359f97262
obo1412 <obo1412@gmail.com>
2025-11-14 23:43:25 +0900

    가까스로 한스텝

--- src/routes/clients/clientsList.svelte
+++ src/routes/clients/clientsList.svelte
@@ -1,6 +1,10 @@
 <script context="module">
 	export const clientList = [
 		{ company: 'DY Auto', machinery: ['PWM #16 LINE', 'RTR #3 LINE'] },
-		{ company: '두원공조', machinery: ['ROTOR #1 LINE', 'ROTOR #2 LINE'] }
+		{ company: '두원공조', machinery: ['ROTOR #1 LINE', 'ROTOR #2 LINE'] },
+		{
+			company: '아이탑스(ITOPS AUTOMOTIVE)',
+			machinery: ['ACTUATOR CURLING(현대)']
+		}
 	];
 </script>
